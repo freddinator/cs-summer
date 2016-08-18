f8de975b5d756dee90aa252a8cb0f2d8bfed4575
Freddy Heppell <f.heppell@gmail.com>
2016-08-18 11:52:05 +0100

    removed unneeded regexp modifier

--- plate.py
+++ plate.py
@@ -1,6 +1,7 @@
 import re
 
 numberplate = input("Enter numberplate > ")
+# Strip all spaces from the entered text
 numberplate = numberplate.replace(" ", "")
 
 re1='([a-z])'	# Any character
@@ -11,7 +12,8 @@ re5='([a-z])'
 re6='([a-z])'
 re7='([a-z])'
 
-rg = re.compile(re1+re2+re3+re4+re5+re6+re7,re.IGNORECASE|re.DOTALL)
+# Compile the regexp, ignore case
+rg = re.compile(re1+re2+re3+re4+re5+re6+re7,re.IGNORECASE)
 
 if rg.match(numberplate) and len(numberplate) == 7:
 	print ("\033[92m ✔ Valid\033[0m")
