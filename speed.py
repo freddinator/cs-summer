aeecda8a332caa52089976acac84e13057dee07c
Freddy Heppell <f.heppell@gmail.com>
2016-08-18 11:52:27 +0100

    added rounding to output speed and non-int catching

--- speed.py
+++ speed.py
@@ -11,24 +11,33 @@ print("Converting 100 m/s to mph:", mph)
 ms = mph_to_mps(mph)
 print("Converting back to m/s:", ms)
 print("Difference:", 100-ms)
-print("--------------\n")
+print("--------------")
 
 distance = float(input('How long is the road distance in metres? > '))
 print('  \033[92m ✔ Road distance set\033[0m')
 
 speedlimit = float(input('Enter speed limit in miles per hour > '))
 print('  \033[92m ✔ Speed limit set\033[0m')
+print("--------------")
 
 # d = s*t
 # s = d/t
 while True:
-	print("--------------\n")
-	time = float(input('Enter time to get through the road distance in seconds > '))
-	speed_in_ms = distance / time
-	speed_in_mph = mps_to_mph(speed_in_ms)
-	print('Speed in MPH:', speed_in_mph)
-	if speed_in_mph < speedlimit or speed_in_mph == speedlimit:
-		print('\033[92m ✔ Under speed limit \033[0m')
+	try:
+		time = float(input('Enter time to get through the road distance in seconds > '))
+	except ValueError:
+		print('\033[93m ❗ Invalid time \033[0m')
 	else:
-		print('\033[91m ✗ Over speed limit \033[0m')
+		speed_in_ms = distance / time
+		speed_in_mph = mps_to_mph(speed_in_ms)
+		print('Speed in MPH: {0:.2f}'.format(speed_in_mph))
+		if speed_in_mph < 0.05:
+			print('\033[93m ⓘ  The driver\'s speed is zero when rounded to two decimal places \033[0m')
+		elif speed_in_mph < speedlimit or speed_in_mph == speedlimit:
+			print('\033[92m ✔ Under speed limit \033[0m')
+		else:
+			print('\033[91m ✗ Over speed limit \033[0m')
+
+		print("--------------\n")
+
 
